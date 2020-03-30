aafdc4d3b200331a0aa9412d2692ab8a4ce45f6b
Do Xuan Duy <dxbduy@tma.com.vn>
2020-03-30 16:16:21 +0700

    abc

--- PullRequest.sh
+++ PullRequest.sh
@@ -1,37 +1,18 @@
 
 #!/bin/bash
-to_branch=$1
-upstreamURL=$2
-BranchOrigin=$3
-upstream='https://github.com/duydoxuan/test-ray.git'
-pull_request() {
-  
-  if [ -z $to_branch ]; then
-    to_branch="master"
-  fi
-  # fetch latest code 
-  git remote add upstream 'https://github.com/duydoxuan/test-ray.git'
-  git checkout jenkins
-  git fetch upstream
-  git merge upstream/master
-  # try the upstream branch if possible, otherwise origin will do
 
 
-  upstream=$(git config --get remote.upstream.url)
-  origin=$(git config --get remote.origin.url)
-  if [ -z $upstream ]; then
-    upstream=$origin
-  fi
-  
-  to_user=$(echo $upstream | sed -e 's/.*[\/:]\([^/]*\)\/[^/]*$/\1/')
-  from_user=$(echo $origin | sed -e 's/.*[\/:]\([^/]*\)\/[^/]*$/\1/')
-  repo=$(basename `git rev-parse --show-toplevel`)
-  from_branch=$(git rev-parse --abbrev-ref HEAD)
-}
- 
-# usage
-set -x 
 
-pull_request              # PR to master
-pull_request other_branch upstreamURL BranchOrigin  # PR to other_branch\
+#config user
+git config --global user.name "jenkins"
+git config --global user.email "jenkins@humana.com"
+
+git checkout jenkins
+
+git add .
+git commit -am "test"
+git push origin
+
+
+
 
