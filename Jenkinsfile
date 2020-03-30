acbd8c238f723681c6c790574934880e26997d56
Do Xuan Duy <dxbduy@tma.com.vn>
2020-03-31 01:12:00 +0700

    abc

--- Jenkinsfile
+++ Jenkinsfile
@@ -166,9 +166,9 @@ def main(){
             writeJSON file: 'ver.json.pre', json: jsonResult, pretty: 5
             // git 
             try {
-            withCredentials([usernamePassword(credentialsId: 'de74115a-88ca-446e-aac1-fb8e0122f528', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
+            // withCredentials([usernamePassword(credentialsId: 'de74115a-88ca-446e-aac1-fb8e0122f528', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
                 sh(script: "chmod 755 PullRequest.sh && ./PullRequest.sh ${USERNAME} ${PASSWORD}")
-                    }                    
+                    // }                    
                 } catch(Exception ex){
                     cleanWs()
                 }
--- PullRequest.sh
+++ PullRequest.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 set -x
-user_name=$1
-password=$2
+# user_name=$1
+# password=$2
 # config user
-git config --global user.name "jenkins"
-git config --global user.email "jenkins@humana.com"
+git config --global user.name "duydoxuan"
+git config --global user.email "duydxbit@gmail.com"
 #clone form kubertest
-git clone -b jenkins https://"${user_name}:${password}"@github.com/novemberrain-test/test-ray.git
+git clone -b jenkins git@github.com:novemberrain-test/test-ray.git
 cd test-ray
-git remote add upstream https://github.com/novemberrain-test/test-ray.git
+git remote add upstream git@github.com:duydoxuan/test-ray.git
 git remote -v 
 # fetch latest code from upstream
 git fetch upstream
@@ -17,7 +17,7 @@ git merge upstream/master
 mv ver.json ../ver.json.bak
 mv ../ver.json.pre ver.json
 # push code
-git remote set-url origin git@github.com:novemberrain-test/test-ray.git
+# git remote set-url origin git@github.com:novemberrain-test/test-ray.git
 git add ver.json
 git commit -am "jenkins"
 git push origin
