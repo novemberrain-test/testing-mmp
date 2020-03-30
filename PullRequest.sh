c2104907d56f7fe3ce21d91c817a04066603f830
Do Xuan Duy <dxbduy@tma.com.vn>
2020-03-30 22:34:58 +0700

    abc

--- PullRequest.sh
+++ PullRequest.sh
@@ -10,4 +10,4 @@ git push "https://${user_name}:${password}@github.com/novemberrain-test/test-ray
 
 # Create pull request
 
-curl -s -H "Authorization: token 72c15752b8eb9f084ae2b1dbd3c4989a3446f469" https://api.github.com/repos/duydoxuan/test-ray/pulls/
+curl -s -H "Authorization: token 72c15752b8eb9f084ae2b1dbd3c4989a3446f469"  -d '{"title": "jenkins update sprint, version","head": "octocat:new-feature","base": "master"}' https://api.github.com/repos/duydoxuan/test-ray/pulls/
